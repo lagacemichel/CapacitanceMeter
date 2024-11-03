96b6bc61193d30c53f58be35288e84cda4965bba
Michel Lagacé <lagacemichel@hotmail.com>
2024-11-03 08:41:55 -0500

    555 Timer IC in astable mode and LCD display.
    New capacitance measurement using 555 Timer IC in astable mode. Capacitance is displayed on liquid crystal display directly connected to Arduino digital pins.

--- CapacitanceMeter.ino
+++ CapacitanceMeter.ino
@@ -1,17 +1,17 @@
-
 /*
   Compute capacitor value using NE555 timer sketch
   
-  We use an NE555 timer chip in monostable configuration to charge a capacitor
-  through a known resistor value and measure the time it takes for the capacitor's
-  charge to reach the NE555 internal threshold of 2/3 Vcc. We use the time to
-  compute the capacitance value and return it to the PC connected to the Arduino
-  board. It is associated with the Capacitance Meter Using the NE555 Timer blog
-  post on https://lagacemichel.com
+  We use a NE555 timer integrated circuit in astable configuration to charge
+  and discharge a capacitor through known resistor values and measure the time
+  it takes for the circuit to go through a full cycle between the NE555
+  internal thresholds of 1/3 Vcc and 2/3 Vcc. We use the resulting time to
+  compute the capacitance value and return it to the PC connected to the
+  Arduino board. It is associated with the Capacitance Meter Using the NE555
+  Timer IC blog post on https://lagacemichel.com
 
   MIT License
 
-  Copyright (c) 2023, Michel Lagace
+  Copyright (c) 2024, Michel Lagace
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
@@ -20,8 +20,8 @@
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
 
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
 
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -32,49 +32,71 @@
   SOFTWARE.
 */
 
-const int NE555ResetPin = 4;
-const int NE555OutputPin = 3;
-const int NE555TriggerPin = 2;
-float timerResistorValue = 100900.0;
-float RCToThresholdTimeRatio = 1.10363832;
-int codeExecutionTimeOverhead = 16;
-const int capacitorDischargeWaitTime = 1000;
+#include <LiquidCrystal.h>
+const int NE555OutputPin = 2;
+const float timerResistorValueInOhms = 69100.0;
+const float dischargeResistorValueInOhms = 121900.0;
+const float RCToFullCycleTimeRatio = 1.442295040888963;
+const int waitTimeInMilliseconds = 500;
+volatile bool cycleComplete = false;
+volatile unsigned long previousRisingEdgeTimeInMicroseconds = 0;
+volatile unsigned long curentRisingEdgeTimeInMicroseconds = 0;
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
-void resetCapacitanceMeter() {
-  Serial.println("Reseting Capacitance Meter");
-  pinMode(NE555ResetPin, OUTPUT);
-  digitalWrite(NE555ResetPin, LOW);
-  digitalWrite(NE555ResetPin, HIGH);
-  delay(capacitorDischargeWaitTime);
+void DisplayStringsOnLcd(const String& textRow1, const String& textRow2) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(textRow1);
+  lcd.setCursor(0, 1);
+  lcd.print(textRow2);
 }
 
-void triggerTimerAndWaitForThreshold() {
-  digitalWrite(NE555TriggerPin, LOW);
-  digitalWrite(NE555TriggerPin, HIGH);
-  while(digitalRead(NE555OutputPin) == HIGH) {
+void prepareLcd() {
+  lcd.begin(16, 2);
+  lcd.clear();
+  DisplayStringsOnLcd("Measuring", "");
+}
+
+inline float calculateCapacitance(unsigned long timeForFullCycleInMicroseconds) {
+  return timeForFullCycleInMicroseconds * RCToFullCycleTimeRatio / (timerResistorValueInOhms + (2.0 * dischargeResistorValueInOhms));
+}
+
+void onRisingEdge() {
+  previousRisingEdgeTimeInMicroseconds = curentRisingEdgeTimeInMicroseconds;
+  curentRisingEdgeTimeInMicroseconds = micros();
+  cycleComplete = previousRisingEdgeTimeInMicroseconds > 0;
+}
+
+void addCapacitanceToString (float Capacitance, String& capacitanceStr) {
+  if (Capacitance >= 1.0) {
+    capacitanceStr += String(Capacitance, 2) + " uF";
+  }
+  else if (Capacitance >= 0.001) {
+    capacitanceStr += String(Capacitance * 1000.0, 2) + " nF";
+  }
+  else {
+    capacitanceStr += String(Capacitance * 1000000.0, 2) + " pF";
   }
 }
 
-void returnCapacitanceToPc(float capacitance) {
-  Serial.print("Capacitance: ");
-  Serial.print(capacitance, 4);
-  Serial.println(" uF");
+void displayCapacitanceOnLcd(float Capacitance) {
+  String capacitorStr = "Cx: ";
+  addCapacitanceToString(Capacitance, capacitorStr);
+  DisplayStringsOnLcd(capacitorStr,"");
 }
 
 void setup() {
-  Serial.begin(9600);
   pinMode(NE555OutputPin, INPUT);
-  pinMode(NE555TriggerPin, OUTPUT);
-  digitalWrite(NE555TriggerPin, HIGH);
-  resetCapacitanceMeter();
-  Serial.println("Measuring Capacitance");
+  attachInterrupt(digitalPinToInterrupt(NE555OutputPin), onRisingEdge, RISING);
+  prepareLcd();
 }
 
 void loop() {
-  long int initialTimerValue = micros();
-  triggerTimerAndWaitForThreshold();
-  long int timeToReachThreshold = micros() - initialTimerValue - codeExecutionTimeOverhead;
-  float capacitance = timeToReachThreshold/RCToThresholdTimeRatio/timerResistorValue;
-  returnCapacitanceToPc(capacitance);
-  delay(capacitorDischargeWaitTime);
+  if (cycleComplete) {
+    unsigned long timeForFullCycleInMicroseconds = curentRisingEdgeTimeInMicroseconds - previousRisingEdgeTimeInMicroseconds;
+    float capacitanceValue = calculateCapacitance(timeForFullCycleInMicroseconds);
+    displayCapacitanceOnLcd(capacitanceValue);
+    delay(waitTimeInMilliseconds);
+    cycleComplete = false;
+  }
 }
